1d82c98dd503740ddbf033d84543b9025b338a36
alif-dot <alif17648@gmail.com>
2023-06-03 23:40:55 +0600

    Add project files.

---
+++ DropDownList_inMVC5/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using DropDownList_inMVC5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownList_inMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {           
+            var countries = new List<Country>()
+            {
+                new Country(){ Id=1, Text = "Bangladesh"},
+                new Country(){ Id=1, Text = "Japan"},
+                new Country(){ Id=1, Text = "China"},
+                new Country(){ Id=1, Text = "Indonesia"},
+                new Country(){ Id=1, Text = "Turkey"},
+                new Country(){ Id=1, Text = "Afganistan"},
+                new Country(){ Id=1, Text = "Pakistan"},
+                new Country(){ Id=1, Text = "Saudi Arabia"}
+            };
+
+            ViewBag.list = countries;
+
+            //Employee emp = new Employee()
+            //{
+            //    Country = "Bangladesh"
+            //};
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Test(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Test");
+            }
+
+            var countries = new List<Country>()
+            {
+                new Country(){ Id=1, Text = "Bangladesh"},
+                new Country(){ Id=1, Text = "Japan"},
+                new Country(){ Id=1, Text = "China"},
+                new Country(){ Id=1, Text = "Indonesia"},
+                new Country(){ Id=1, Text = "Turkey"},
+                new Country(){ Id=1, Text = "Afganistan"},
+                new Country(){ Id=1, Text = "Pakistan"},
+                new Country(){ Id=1, Text = "Saudi Arabia"}
+            };
+
+            ViewBag.list = countries;
+
+            return View();
+        }
+    }
+
+    class Country
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ DropDownList_inMVC5/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DropDownList_inMVC5.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+    }
+}
